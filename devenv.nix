63ce9d1aaacec18625b132f0bb70ab1a14d8659a
Amir Farzamnia <amirfarzamnia@proton.me>
2026-07-14 11:36:18 +0330

    chore: minor improvements

--- devenv.nix
+++ devenv.nix
@@ -27,13 +27,13 @@
       echo "Processing and formatting palettes into '$TARGET_DIR'..."
 
       for yaml in "$TMP_CLONE/base16"/*.yaml; do
-          [ -e "$yaml" ] || continue
-          filename=$(basename "$yaml" .yaml)
+        [ -e "$yaml" ] || continue
+        filename=$(basename "$yaml" .yaml)
 
-          yq -o=json "$yaml" | jq -r '
-            "#!/bin/sh\n\n# Scheme: \(.name) (\(.variant))\n# Author: \(.author)\n\n" +
-            (.palette | to_entries | map(select(.key | test("^base0[0-9A-Fa-f]$")) | "export \(.key)=\"\(.value)\"") | join("\n"))
-          ' > "$TARGET_DIR/$filename.sh"
+        yq -o=json "$yaml" | jq -r '
+          "#!/bin/sh\n\n# Scheme: \(.name) (\(.variant))\n# Author: \(.author)\n\n" +
+          (.palette | to_entries | map(select(.key | test("^base0[0-9A-Fa-f]$")) | "export \(.key)=\"\(.value)\"") | join("\n"))
+        ' > "$TARGET_DIR/$filename.sh"
       done
 
       rm -rf "$TMP_CLONE"
