3020271c5e03a0342281270bf395991e37a38330
Amir Farzamnia <amirfarzamnia@proton.me>
2026-07-14 20:09:06 +0330

    feat: add horizontal-stripes style

---
+++ styles/horizontal-stripes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# shellcheck disable=SC2046
+magick $(get_palette_expr "-append") \
+	-filter point -resize "${WIDTH}x${HEIGHT}!" \
+	-resize "${TARGET_WIDTH}x${TARGET_HEIGHT}!" "$OUTPUT_FILE"
